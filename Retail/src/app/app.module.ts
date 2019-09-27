46419a82108bb764ff6fa9657f3d5aa4b0fa6dda
Elkhan Mursali <elxanmursal@gmail.com>
2019-09-27 09:32:25 +0200

    Adding empty module

--- Retail/src/app/app.module.ts
+++ Retail/src/app/app.module.ts
@@ -3,30 +3,26 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from "./app-routing.module";
-
 import { AngularFontAwesomeModule } from "angular-font-awesome";
 
+import { AppRoutingModule } from "./app-routing.module";
+import { SaleModule } from "./main/sale.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./main/home.component";
 import { HeaderComponent } from "./main/header/header.component";
-import { SaleItemsComponent } from "./main/sale-items/sale-items.component";
-import { SaleButtonsComponent } from "./main/sale-buttons/sale-buttons.component";
-import { SaleListComponent } from "./main/sale-list/sale-list.component";
-import { SaleItemComponent } from "./main/sale-items/sale-item/sale-item.component";
 import { ItemComponent } from "./items/item/item.component";
 import { ItemsComponent } from "./items/items.component";
+import { SaleItemsComponent } from "./main/sale-items/sale-items.component";
+import { SaleButtonsComponent } from "./main/sale-buttons/sale-buttons.component";
+import { SaleItemComponent } from "./main/sale-items/sale-item/sale-item.component";
+import { SaleListComponent } from "./main/sale-list/sale-list.component";
 import { ModalWindowComponent } from "./dialog-box/modal-window/modal-window.component";
 import { ConfirmationComponent } from "./dialog-box/confirmation/confirmation.component";
 import { SignInComponent } from "./sign-in/sign-in.component";
-
 import { HighlightDirective } from "./directives/highlight.directive";
-
 import { CommunicationService } from "./services/communication.service";
-
 import { AuthGuard } from "./guards/auth.guard";
 import { LockOutGuard } from "./guards/lock-out.guard";
-
 import { ShortenPipe } from "./helpers/pipes/shorten.pipe";
 import { FilterPipe } from "./helpers/pipes/filter.pipe";
 
@@ -35,12 +31,12 @@ import { FilterPipe } from "./helpers/pipes/filter.pipe";
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    SaleItemsComponent,
-    SaleButtonsComponent,
-    SaleListComponent,
-    SaleItemComponent,
     ItemComponent,
     ItemsComponent,
+    SaleItemsComponent,
+    SaleButtonsComponent,
+    SaleItemComponent,
+    SaleListComponent,
     ModalWindowComponent,
     ConfirmationComponent,
     SignInComponent,
@@ -50,11 +46,12 @@ import { FilterPipe } from "./helpers/pipes/filter.pipe";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
     AngularFontAwesomeModule,
-    HttpClientModule
+    SaleModule
   ],
   providers: [CommunicationService, AuthGuard, LockOutGuard],
   bootstrap: [AppComponent]
---
+++ Retail/src/app/main/sale.module.ts
@@ -0,0 +1,9 @@
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, FormsModule]
+})
+export class SaleModule {}
